6d8d1b003776120e2ae5b710be990b85fdc5ee89
Florent Moulin <florent@MacBook-de-Florent.local>
2024-06-22 02:48:54 +0200

    product frontend ok

---
+++ src/components/Modal_Product.jsx
@@ -0,0 +1,13 @@
+import "./Modal_Product.css"
+
+function Modal_Product () {
+    return <div className="modal_product">
+        <div className="modal_fixed">
+                <button>Contacter</button>
+                <button className="black_btn">Acheter</button>
+           </div>
+    </div>
+    
+}
+
+export default Modal_Product;
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header"
 import Product_Component from "../components/Product_Component";
+import Modal_Product from "../components/Modal_Product";
 import "./Product.css"
 
 function Product() {
     return <div>
             <Header />
             <Product_Component />
+            <Modal_Product />
         </div>
         
 }
